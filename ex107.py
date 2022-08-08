9449ab2bdd211aaaef23f832db215711fc76dd96
Eliezer Tavares <tavares.eliezer@yahoo.com>
2022-08-07 21:19:17 -0300

    módulos1

--- ex107.py
+++ ex107.py
@@ -1,6 +1,17 @@
 from ex107mod import func
 
-num = 35
+num = 20
+
+func.titulo(f"A metade de {num} é:")
 print(func.metade(num))
+
+func.titulo(f"O dobro de {num} é:")
 print(func.dobro(num))
-print(num*0.1)
+
+per = 20
+
+func.titulo(f"Aumentar {per}% ao número {num}:")
+print(func.aumentar(num,per))
+
+func.titulo(f"Diminui {per}% ao número {num}:")
+print(func.diminuir(num,per))
--- ex107mod/func.py
+++ ex107mod/func.py
@@ -3,3 +3,15 @@ def metade(n):
 
 def dobro(n):
     return n*2
+
+def aumentar(n,pc):
+    calc = n*(pc*0.01)
+    return n+calc
+
+def diminuir(n,pc):
+    calc = n*(pc*0.01)
+    return n-calc
+
+def titulo (text):
+    print("="*40)
+    print(f"{text}")
