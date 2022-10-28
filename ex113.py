194f0eb5a153f64789ec4954c2fa2f6e4a7e62ff
Eliezer Tavares <tavares.eliezer@yahoo.com>
2022-10-28 04:39:35 -0300

    revisions

--- ex113.py
+++ ex113.py
@@ -1,4 +1,20 @@
-from ex113_functions import leia_int, leia_float
+def leia_int(frase):
+    while True:
+        try:
+            leia = int(input(f"{frase}"))
+            return leia
+        except Exception as erro:
+            print(f"Não é um número inteiro: {erro.__cause__}")
+
+
+def leia_float(frase):
+    while True:
+        try:
+            leia = float(input(f"{frase}"))
+            return leia
+        except Exception as erro:
+            print(f"Não é um número real: {erro.__cause__}")
+
 
 num_int = leia_int("Digite um número INTEIRO: ")
 num_float = leia_float("Digite um número REAL: ")
